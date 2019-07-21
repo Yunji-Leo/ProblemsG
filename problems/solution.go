f4d64136052d0f698faf0be9161406d4dda5a269
Leo Zhong <yunji@uber.com>
2019-07-20 23:04:10 -0700

    4sum

--- problems/solution.go
+++ problems/solution.go
@@ -638,3 +638,38 @@ func letterCombinationsRecursive(result *[]string, phone map[uint8][]string, dig
 		temp = temp[:len(temp)-1]
 	}
 }
+
+func fourSum(nums []int, target int) [][]int {
+	sort.Ints(nums)
+	result := make([][]int, 0)
+	for i := 0; i < len(nums)-3; i++ {
+		if i == 0 || nums[i] != nums[i-1] {
+			for j := i + 1; j < len(nums)-2; j++ {
+				if j == i+1 || nums[j] != nums[j-1] {
+					left := j + 1
+					right := len(nums) - 1
+					for left < right {
+						sum := nums[i] + nums[j] + nums[left] + nums[right]
+						if sum > target {
+							right--
+						} else if sum < target {
+							left++
+						} else {
+							ans := []int{nums[i], nums[j], nums[left], nums[right]}
+							result = append(result, ans)
+							for left < right && nums[left] == nums[left+1] {
+								left++
+							}
+							for left < right && nums[right] == nums[right-1] {
+								right--
+							}
+							left++
+							right--
+						}
+					}
+				}
+			}
+		}
+	}
+	return result
+}
